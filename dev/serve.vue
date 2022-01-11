5e162034bbd1bbd26d294fe82c31292e1588a1a5
Talentedunicorn <dev@talentedunicorn.com>
2022-01-11 17:01:26 +0800

    Initial

---
+++ dev/serve.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Vue from "vue";
+import sampleDoc from "./test-data.json";
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Keystone6VueRenderer } from '@/entry.esm';
+
+export default Vue.extend({
+  name: "ServeDev",
+  data() {
+    return {
+      doc: sampleDoc,
+    };
+  },
+  // components: {
+  //  Keystone6VueRenderer,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <Document :document="doc" />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: sans-serif;
+}
+</style>
